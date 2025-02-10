4ad65e1c3d4c16e4b06153defb0052d292648889
lavkush <lkyadav090@gmail.com>
2025-02-10 08:34:44 +0530

    env variable added

--- src/lib/api.tsx
+++ src/lib/api.tsx
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = "http://localhost:3000";
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export function api() {
   const getData = async (url: string) => {
     const apiUrl = `${BASE_URL}${url}`;
